16ebda1764b987990d7ff520b05d9cf214cfe300
Anamika Tiwari <155213271+Anamikatiwari@users.noreply.github.com>
2024-03-29 13:33:01 +0545

    edit student

--- crudapp/views.py
+++ crudapp/views.py
@@ -23,7 +23,34 @@ class Add_Student(View):
             return redirect('/')
         else:
             return render(request,'core/add-student.html' ,{'form':fm})
-            
+        
+        
+class Delete_Student(View): 
+    def post(self, request):
+        data = request.POST
+        id = data.get('id')   
+        studata = Student.objects.get(id=id) 
+        studata.delete()
+        return redirect('/')
+
+class EditStudent(View):
+    def get(self, request, id):
+       stu =Student.objects.get(id=id)
+       fm = AddStudentForm(instance=stu) 
+       return render(request, 'core/edit-student.html', {'form':fm})
+   
+    def post(self, request, id):
+       stu = Student.objects.get(id=id)
+       fm = AddStudentForm(request.POST, instance=stu)
+       if fm.is_valid():
+            fm.save()  
+            return redirect('/')  
+            return render(request, 'core/edit-student.html', {'form': fm})  
+                
+   
+    
+      
+             
 
         
         
